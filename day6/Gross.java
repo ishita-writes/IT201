2f86e787af08f927a8a7263fc4e3712bad7fb4ad
ishita-writes <ishitaghoshhazra@gmail.com>
2026-04-09 21:38:27 +0530

    Update Gross.java

--- day6/Gross.java
+++ day6/Gross.java
@@ -1,50 +1,60 @@
 import java.util.Scanner;
-interface Gross
+interface Gross 
 {
-    double TA = 10.0;
-    double DA = 15.0;
+    double TA_PERCENT = 10.0; 
+    double DA_PERCENT = 15.0; 
     void calculate_GrossSalary();
 }
 
-class Employee
-{
+class Employee {
     protected String name;
     protected int EmpID;
-
-    public void getData()
-    {
-        System.out.println("Enter the name of the employee : ");
+    Scanner sc = new Scanner(System.in);
+    public void getData() {
+        System.out.print("Enter the name of the employee: ");
         name = sc.nextLine();
-        System.out.println("Enter the Employee ID : ");
+        System.out.print("Enter the Employee ID: ");
         EmpID = sc.nextInt();
     }
 
-    public void displayProfile()
-    {
-        System.out.println("Employee Name : "+name);
-        System.out.println("Employee ID : "+EmpID);
+    public void displayProfile() {
+        System.out.println("\n--- Employee Profile ---");
+        System.out.println("Employee Name : " + name);
+        System.out.println("Employee ID   : " + EmpID);
     }
 }
 
-class Salary extends Employee implements Gross
-{
+class Salary extends Employee implements Gross {
     private double BasicPay;
     private double GrossPay;
 
-    public void SalaryData()
-    {
-        System.out.println("Basic Pay : "+BasicPay);
-        System.out.println("Gross Pay : "+GrossPay);
+    public void getSalaryInput() {
+        System.out.print("Enter Basic Pay: ");
+        BasicPay = sc.nextDouble();
     }
 
     @Override
-    public void calculate_GrossSalary()
-    {
-        double ta = ()
+    public void calculate_GrossSalary() {
+        double ta_amount = (TA_PERCENT / 100) * BasicPay;
+        double da_amount = (DA_PERCENT / 100) * BasicPay;
+        GrossPay = BasicPay + ta_amount + da_amount;
+    }
+
+    public void displaySalaryData() {
+        System.out.println("Basic Pay     : Rs." + BasicPay);
+        System.out.println("TA (" + TA_PERCENT + "%)    : Rs." + (TA_PERCENT / 100 * BasicPay));
+        System.out.println("DA (" + DA_PERCENT + "%)    : Rs." + (DA_PERCENT / 100 * BasicPay));
+        System.out.println("Gross Pay     : Rs." + GrossPay);
     }
 }
 
-public class Gross
-{
-    
+public class Main {
+    public static void main(String[] args) {
+        Salary empSalary = new Salary();
+        empSalary.getData();
+        empSalary.getSalaryInput();
+        empSalary.calculate_GrossSalary();
+        empSalary.displayProfile();
+        empSalary.displaySalaryData();
+    }
 }
