87501208cffa0e852a29035a3231a6132c553641
ishita-writes <ishitaghoshhazra@gmail.com>
2026-04-19 01:03:55 +0530

    Update Asset.java

--- day7/Asset.java
+++ day7/Asset.java
@@ -2,67 +2,50 @@ import java.util.Scanner;
 abstract class Asset {
     String descriptor, date;
     double currentValue;
-
     Asset(String desc, String d, double val) {
         this.descriptor = desc;
         this.date = d;
         this.currentValue = val;
     }
-
-    abstract void displayDetails();
+abstract void displayDetails();
 }
-
 class Stock extends Asset {
     int numShares;
     double sharePrice;
     String asset;
-
-    Stock(String desc, String d, double val, int n, double p, String a) {
-        super(desc, d, val);
-        this.numShares = n;
-        this.sharePrice = p;
-        this.asset = a;
+  Stock(String desc, String d, double val, int n, double p, String a) {
+        super(desc, d, val); this.numShares = n; this.sharePrice = p; this.asset = a;
     }
-
     void displayDetails() {
         System.out.println("\n--- Stock Asset ---");
         System.out.println("Desc: " + descriptor + " | Date: " + date + " | Value: " + currentValue);
         System.out.println("Shares: " + numShares + " | Price: " + sharePrice + " | Asset: " + asset);
     }
 }
-
 class Bond extends Asset {
     double interestRate;
     String asset;
-
     Bond(String desc, String d, double val, double rate, String a) {
-        super(desc, d, val);
-        this.interestRate = rate;
-        this.asset = a;
+        super(desc, d, val); this.interestRate = rate; this.asset = a;
     }
-
     void displayDetails() {
         System.out.println("\n--- Bond Asset ---");
         System.out.println("Desc: " + descriptor + " | Value: " + currentValue + " | Interest: " + interestRate + "%");
     }
 }
-
 class Savings extends Asset {
     double interestRate;
     String asset;
-
     Savings(String desc, String d, double val, double rate, String a) {
         super(desc, d, val);
         this.interestRate = rate;
         this.asset = a;
     }
-
     void displayDetails() {
         System.out.println("\n--- Savings Asset ---");
         System.out.println("Desc: " + descriptor + " | Rate: " + interestRate + "% | Asset Type: " + asset);
     }
 }
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
