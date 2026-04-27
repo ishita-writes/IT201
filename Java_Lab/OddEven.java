e280c17f98d36206b62e5116338b951c8fb635f7
ishita-writes <ishitaghoshhazra@gmail.com>
2026-04-27 16:08:12 +0530

    Create OddEven.java

---
+++ Java_Lab/OddEven.java
@@ -0,0 +1,18 @@
+class OddEven
+{
+    int num;
+    void check()
+    {
+        if (num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+    
+    public static void main(String args[])
+    {
+        OddEven obj = new OddEven();
+        obj.num = 45;
+        obj.check();
+    }
+}
