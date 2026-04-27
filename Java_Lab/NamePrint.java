b0badf25b0460915d87514844558bc57de651ce7
ishita-writes <ishitaghoshhazra@gmail.com>
2026-04-27 16:07:51 +0530

    Create NamePrint.java

---
+++ Java_Lab/NamePrint.java
@@ -0,0 +1,15 @@
+class NamePrint
+{
+    String name; 
+    void PrintName()
+    {
+        System.out.println("My name is "+name);
+    }
+
+    public static void main(String args[])
+    {
+        NamePrint obj = new NamePrint();
+        obj.name = "Ishita";
+        obj.PrintName();
+    }
+}
